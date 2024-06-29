21866bef4df213615ff19a9f2d91677f7dc085ae
Devil-Dude <90880482+skull-fir@users.noreply.github.com>
2024-06-29 16:55:01 +1000

    Update loop.js

--- buttons/loop.js
+++ buttons/loop.js
@@ -3,10 +3,10 @@ const { Translate } = require('../process_tools');
 
 module.exports = async ({ inter, queue }) => {
     const methods = ['disabled', 'track', 'queue'];
-    if (!queue?.isPlaying()) return inter.editReply({ content: await Translate(`No music currently playing... try again ? <❌>`) });
+    if (!queue?.isPlaying()) return inter.editReply({ content: await Translate(`No music currently playing... try again ? <:1326_cross:908665425083109416>`) });
 
     if (queue.repeatMode === 2) queue.setRepeatMode(QueueRepeatMode.OFF)
     else queue.setRepeatMode(queue.repeatMode + 1)
 
-    return inter.editReply({ content: await Translate(`Loop made has been set to <**${methods[queue.repeatMode]}**>.<✅>`) });
+    return inter.editReply({ content: await Translate(`Loop made has been set to <**${methods[queue.repeatMode]}**>.<:1779_check:908664839973502986>`) });
 }
