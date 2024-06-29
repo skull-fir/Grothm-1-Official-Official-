41583a635c1f98e15a8db7a42d74876ed40cd062
Devil-Dude <90880482+skull-fir@users.noreply.github.com>
2024-06-29 16:54:25 +1000

    Update back.js

--- buttons/back.js
+++ buttons/back.js
@@ -3,16 +3,16 @@ const { Translate } = require('../process_tools');
 module.exports = async ({ inter, queue }) => {
   if (!queue?.isPlaying())
     return inter.editReply({
-      content: await Translate(`No music currently playing... try again ? <❌>`),
+      content: await Translate(`No music currently playing... try again ? <:1326_cross:908665425083109416>`),
     });
   if (!queue.history.previousTrack)
     return inter.editReply({
-      content: await Translate(`There was no music played before <${inter.member}>... try again ? <❌>`),
+      content: await Translate(`There was no music played before <${inter.member}>... try again ? <:1326_cross:908665425083109416>`),
     });
 
   await queue.history.back();
 
   inter.editReply({
-    content: await Translate(`Playing the <**previous**> track <✅>`),
+    content: await Translate(`Playing the <**previous**> track <:1779_check:908664839973502986>`),
   });
 };
